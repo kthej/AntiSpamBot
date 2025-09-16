7decaa7221c4dfefce736eebec745bee7ff1ec93
kthej <kadenjones1212@gmail.com>
2025-09-16 17:32:47 -0600

    removed all token mentions

--- handler.py
+++ handler.py
@@ -27,32 +27,31 @@ FLAGGED_PHRASES = (
 GROUPME_TOKEN = os.environ.get("GROUPME_TOKEN")
 
 
-def get_memberships(group_id, token):
-    response = requests.get(f'{API_ROOT}groups/{group_id}', params={'token': token}).json()['response']['members']
+def get_memberships(group_id):
+    response = requests.get(f'{API_ROOT}groups/{group_id}', params={'token': GROUPME_TOKEN}).json()['response']['members']
     return response
 
-def get_membership_id(group_id, user_id, token):
-    memberships = get_memberships(group_id, token)
+def get_membership_id(group_id, user_id):
+    memberships = get_memberships(group_id)
     for membership in memberships:
         if membership['user_id'] == user_id:
             return membership['id']
     return None
 
-def remove_member(group_id, membership_id, token):
-    response = requests.post(f'{API_ROOT}groups/{group_id}/members/{membership_id}/remove', params={'token': token})
+def remove_member(group_id, membership_id):
+    response = requests.post(f'{API_ROOT}groups/{group_id}/members/{membership_id}/remove', params={'token': GROUPME_TOKEN})
     print('Attempted to kick user, got response:')
     print(response.text)
     return response.ok
 
-def delete_message(group_id, message_id, token):
-    response = requests.delete(f'{API_ROOT}conversations/{group_id}/messages/{message_id}', params={'token': token})
+def delete_message(group_id, message_id):
+    response = requests.delete(f'{API_ROOT}conversations/{group_id}/messages/{message_id}', params={'token': GROUPME_TOKEN})
     return response.ok
 
 def kick_user(group_id, user_id):
-    # Use the token from the environment instead of message['token']
-    membership_id = get_membership_id(group_id, user_id, GROUPME_TOKEN)
+    membership_id = get_membership_id(group_id, user_id)
     if membership_id:
-        return remove_member(group_id, membership_id, GROUPME_TOKEN)
+        return remove_member(group_id, membership_id)
     return False
 
 def receive(event, context):
@@ -63,8 +62,8 @@ def receive(event, context):
         if phrase in (message.get('text') or '').lower():
             # Attempt to kick the user using environment token
             if kick_user(message['group_id'], message['user_id']):
-                delete_message(message['group_id'], message['id'], GROUPME_TOKEN)
-                send('Kicked ' + message.get('name', 'user') + ' due to apparent spam post.', bot_id)
+                delete_message(message['group_id'], message['id'])
+                send(f'Kicked {message.get("name", "user")} due to apparent spam post.', bot_id)
             else:
                 print('Kick attempt failed or user is an admin.')
             break
