e116ebe53c4252407bc1c2d50e0a098765651517
kthej <kadenjones1212@gmail.com>
2025-09-16 17:29:33 -0600

    Update handler.py

--- handler.py
+++ handler.py
@@ -9,7 +9,6 @@ import json
 
 API_ROOT = 'https://api.groupme.com/v3/'
 FLAGGED_PHRASES = (
-    
     "selling roc pass",
     "selling my roc pass",
     "selling the roc pass",
@@ -23,6 +22,9 @@ FLAGGED_PHRASES = (
     "selling roc",
     "roc pass"
 )
+
+# Load your GroupMe token from environment variable
+GROUPME_TOKEN = os.environ.get("GROUPME_TOKEN")
 
 
 def get_memberships(group_id, token):
@@ -40,33 +42,29 @@ def remove_member(group_id, membership_id, token):
     response = requests.post(f'{API_ROOT}groups/{group_id}/members/{membership_id}/remove', params={'token': token})
     print('Attempted to kick user, got response:')
     print(response.text)
-    return response.ok  # Return whether the request was successful
-
+    return response.ok
 
 def delete_message(group_id, message_id, token):
     response = requests.delete(f'{API_ROOT}conversations/{group_id}/messages/{message_id}', params={'token': token})
     return response.ok
 
-
-def kick_user(group_id, user_id, token):
-    membership_id = get_membership_id(group_id, user_id, token)
+def kick_user(group_id, user_id):
+    # Use the token from the environment instead of message['token']
+    membership_id = get_membership_id(group_id, user_id, GROUPME_TOKEN)
     if membership_id:
-        return remove_member(group_id, membership_id, token)
+        return remove_member(group_id, membership_id, GROUPME_TOKEN)
     return False
-
-
-
 
 def receive(event, context):
     message = json.loads(event['body'])
-    bot_id = message['bot_id']
+    bot_id = message.get('bot_id')
 
     for phrase in FLAGGED_PHRASES:
-        if phrase in message['text'].lower():
-            # Attempt to kick the user and check if it was successful
-            if kick_user(message['group_id'], message['user_id'], message['token']):
-                delete_message(message['group_id'], message['id'], message['token'])
-                send('Kicked ' + message['name'] + ' due to apparent spam post.', bot_id)
+        if phrase in (message.get('text') or '').lower():
+            # Attempt to kick the user using environment token
+            if kick_user(message['group_id'], message['user_id']):
+                delete_message(message['group_id'], message['id'], GROUPME_TOKEN)
+                send('Kicked ' + message.get('name', 'user') + ' due to apparent spam post.', bot_id)
             else:
                 print('Kick attempt failed or user is an admin.')
             break
@@ -75,7 +73,6 @@ def receive(event, context):
         'statusCode': 200,
         'body': 'ok'
     }
-
 
 def send(text, bot_id):
     url = 'https://api.groupme.com/v3/bots/post'
